06d0d5ae5e462dc45cf63b76a3e95f6f1c98d421
reallydrained <125171490+reallydrained@users.noreply.github.com>
2024-12-02 14:21:15 -0600

    Bitches come and go, bruh
    But you know I stay
    
    // we're almost there =D //

--- appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/init/Materials.java
+++ appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/init/Materials.java
@@ -17,7 +17,7 @@ public class Materials {
 	static {
 		DIRT_ARMOR = EnumHelper.addArmorMaterial("dirt", 2, new int[]{1, 2, 1, 1}, 1);
 		DIRT_ARMOR.customCraftingMaterial = new ItemStack(Blocks.dirt).getItem();
-		DIRT_TOOL = EnumHelper.addToolMaterial("dirt", 0, 21, 1.0f, 0.0f, 1);
+		DIRT_TOOL = EnumHelper.addToolMaterial("dirt", 0, 21, 1.0f, -1.0f, 1);
 		DIRT_TOOL.setRepairItem(new ItemStack(Blocks.dirt));
 	}
 }
