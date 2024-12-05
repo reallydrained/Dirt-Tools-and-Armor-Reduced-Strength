2c14d023f29b6300572d8319cc40888252926ef7
reallydrained <125171490+reallydrained@users.noreply.github.com>
2024-12-04 18:40:25 -0600

    Flash from a clear sky, let the fear fly
    House party nearby, I'm bringing weird guys
    I need some wet wipes and some Clear eyes
    Stop signs and red lights outside Best Buy

--- appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/init/Materials.java
+++ appForgeArchive/1.7.10/src/main/java/com/github/hummel/dirtequipment/init/Materials.java
@@ -18,9 +18,9 @@ public class Materials {
 	static {
 		DIRT_ARMOR = EnumHelper.addArmorMaterial("dirt", 2, new int[]{1, 2, 1, 1}, 1);
 		DIRT_ARMOR.customCraftingMaterial = new ItemStack(Blocks.dirt).getItem();
-		DIRT_TOOL = EnumHelper.addToolMaterial("dirt", 0, 21, 0.9f, -2.0f, 1);
+		DIRT_TOOL = EnumHelper.addToolMaterial("dirt", 0, 21, 1.1f, -2.0f, 1);
 		DIRT_TOOL.setRepairItem(new ItemStack(Blocks.dirt));
-		DIRT_TOOL2 = EnumHelper.addToolMaterial("dirt2", 0, 21, 0.9f, -1.0f, 1);
+		DIRT_TOOL2 = EnumHelper.addToolMaterial("dirt2", 0, 21, 1.1f, -1.0f, 1);
 		DIRT_TOOL2.setRepairItem(new ItemStack(Blocks.dirt));
 	}
 }
